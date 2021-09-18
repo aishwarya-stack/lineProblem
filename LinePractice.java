dc18e9fac9cb780830dee456669eb4849b5daa7f
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-18 11:50:15 +0530

    ADD:new welcome

---
+++ LinePractice.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class LinePractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.print("Welcome to Line Comparison Computation Program on Master Branch");
+	}
+
+}
